974effd1c819cad87ea4cec8adfe5cc6268b7f0d
Chelovek <ntl5551@gmail.com>
2023-04-28 12:34:57 +0300

    commited

--- internal/controllers/http/handlers/updateRecipe.go
+++ internal/controllers/http/handlers/updateRecipe.go
@@ -1,27 +1,18 @@
 package handlers
 
-import (
-	"TestBcraft/internal/adapters/db/postgresadapter"
-	"TestBcraft/internal/controllers/dto"
-	"TestBcraft/internal/domain/service"
-	"TestBcraft/pkg/logger"
-	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
-)
-
-func updateRecipe(c *gin.Context, p *pgxpool.Pool) {
-
-	var updateObject dto.UpdateRecipeDTO
-
-	err := c.ShouldBindJSON(&updateObject)
-	if err != nil {
-		logger.ErrorLogger.Println(err)
-	}
-
-	recipeStorage := postgresadapter.NewRecipeStorage(p)
-
-	recipeService := service.RecipeStorage(recipeStorage)
-
-	err = recipeService.Update()
-
-}
+//func updateRecipe(c *gin.Context, p *pgxpool.Pool) {
+//
+//	var updateObject dto.UpdateRecipeDTO
+//
+//	err := c.ShouldBindJSON(&updateObject)
+//	if err != nil {
+//		logger.ErrorLogger.Println(err)
+//	}
+//
+//	recipeStorage := postgresadapter.NewRecipeStorage(p)
+//
+//	recipeService := service.RecipeStorage(recipeStorage)
+//
+//	err = recipeService.Update()
+//
+//}
